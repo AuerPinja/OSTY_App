0b698fb70498e0482af3f74d4e0ca5ed49456b45
Pinja Auer <pinja.auer@gmail.com>
2024-11-24 22:47:43 +0200

    one more time

--- osty-app/src/assets/firebase.js
+++ osty-app/src/assets/firebase.js
@@ -2,13 +2,15 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore"
 
+/*yritin laittaa nämä tiedot dotenvin kautta, mutta se rikkoi koko sovelluksen eikä ollut aikaa korjata*/
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDMhIzWn106jO4U4sTJh1UU0-wHY7IFQlI",
+  authDomain: "osty-b9b14.firebaseapp.com",
+  projectId: "osty-b9b14",
+  storageBucket: "osty-b9b14.firebasestorage.app",
+  messagingSenderId: "721794808662",
+  appId: "1:721794808662:web:8e188b66d62083da7a34f7"
 };
 
 const app = initializeApp(firebaseConfig);
