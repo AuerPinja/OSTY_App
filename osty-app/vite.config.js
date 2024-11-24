ac90f2f88b2c9dd87009dcce8f214ffe873d4e4b
Pinja Auer <pinja.auer@gmail.com>
2024-11-24 21:29:00 +0200

    publish settings

--- osty-app/vite.config.js
+++ osty-app/vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/osty-app/',
+  base: '/',
   plugins: [react()],
+  build: {
+    assetsDir: 'src',  
+    rollupOptions: {
+      input: 'index.html',
+    }
+  }
 })
